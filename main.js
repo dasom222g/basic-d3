8450294f990f6a6b7bf4fdfacd743c8da42c6872
dasom222g <dasom228@gmail.com>
2025-04-14 13:53:03 +0900

    feat: 반응형으로 특정 값만큼 차지하는게 아닌 퍼센트로 자리 차지

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const fiveSvg = document.querySelector("svg.five");
 fiveSvg.style.width = `550px`;
 fiveSvg.style.height = `${30 * dataset.length}px`;
 
-d3.select("svg") // 그림을 그릴 svg캔버스 불러오기
+d3.select("svg.first") // 그림을 그릴 svg캔버스 불러오기
   .selectAll("text") // 앞으로 생성할 여러가지 text요소 준비
   .data(dataset) // 텍스트요소에 데이터 매핑
   .enter() // 모든 준비 완료되었음을 명시
@@ -88,6 +88,111 @@ d3.select("svg.five")
   .text((d) => d)
   .attr("x", (d) => d + 10)
   .attr("y", (d, i) => i * 30 + 10 + 10)
+  .attr("width", (d) => d)
+  .attr("height", 25)
+  .attr("font-size", "20px")
+  .attr("fill", "black");
+
+// ******************************* //
+// 반응형으로 chart출력
+
+const datasetResponsive = [100, 150, 80, 180, 120];
+const svgWidth = 500;
+const svgHeight = 300;
+
+const svgResponsiveEl = d3.select("svg.responsive");
+
+// xPercent에 대입되는 값은 백분율화 해주는 함수
+const xPercent = d3
+  .scaleLinear() // 비율화 준비
+  .domain([0, d3.max(datasetResponsive)]) // 데이터를 비율화
+  .range([0, svgWidth - 40]); // 데이터가 출력될 프레임을 비율화
+
+svgResponsiveEl
+  .selectAll("rect")
+  .data(datasetResponsive)
+  .enter()
+  .append("rect")
+  .attr("x", 0)
+  .attr("y", (d, i) => i * 25 + 10)
+  .attr("width", (d) => xPercent(d)) // xPercent함수를 이용하여 각 수치값을 전달시 자동으로 제일 큰 수치값을 기준으로 백분율화
+  .attr("height", 20)
+  .attr("fill", "pink");
+
+/**
+ * 미션1: 프레임쪽에서 글자까지 들어간 상태로 백분율화
+ * 미션2: 차트를 오른쪽 정렬 형식으로 출력
+ * 미션3: 차트를 세로형식으로 아래에서 위로 그려지도록 출력
+ */
+
+// 미션1
+svgResponsiveEl
+  .selectAll("text")
+  .data(datasetResponsive)
+  .enter()
+  .append("text")
+  .text((d) => d)
+  .attr("x", (d) => xPercent(d) + 10)
+  .attr("y", (d, i) => i * 25 + 30)
+  .attr("width", (d) => d)
+  .attr("height", 25)
+  .attr("font-size", "20px")
+  .attr("fill", "black");
+
+// 미션2
+const svgResponsiveRightEl = d3.select("svg.responsive-right");
+
+svgResponsiveRightEl
+  .selectAll("rect")
+  .data(datasetResponsive)
+  .enter()
+  .append("rect")
+  .attr("x", (d) => svgWidth - xPercent(d)) // 오른쪽 정렬
+  .attr("y", (d, i) => i * 25 + 10)
+  .attr("width", (d) => xPercent(d))
+  .attr("height", 20)
+  .attr("fill", "blue");
+
+svgResponsiveRightEl
+  .selectAll("text")
+  .data(datasetResponsive)
+  .enter()
+  .append("text")
+  .text((d) => d)
+  .attr("x", (d) => svgWidth - xPercent(d) - 40) // 글자 크기를 포함함 간격
+  .attr("y", (d, i) => i * 25 + 30)
+  .attr("width", (d) => d)
+  .attr("height", 25)
+  .attr("font-size", "20px")
+  .attr("fill", "black");
+
+// 미션3
+const svgResponsiveVerticalEl = d3.select("svg.responsive-vertical");
+
+const yPercent = d3
+  .scaleLinear() // 비율화 준비
+  .domain([0, d3.max(datasetResponsive)]) // 데이터를 비율화
+  .range([0, svgHeight - 40]); // 데이터가 출력될 프레임을 비율화
+
+svgResponsiveVerticalEl
+  .selectAll("rect")
+  .data(datasetResponsive)
+  .enter()
+  .append("rect")
+  .attr("x", (_, i) => i * 25 + 10)
+  .attr("y", (d, i) => svgHeight - yPercent(d))
+  .attr("width", 20)
+  .attr("height", (d) => yPercent(d))
+  .attr("fill", "aqua");
+
+svgResponsiveVerticalEl
+  .selectAll("text")
+  .data(datasetResponsive)
+  .enter()
+  .append("text")
+  .text((d) => d)
+  .attr("x", (d, i) => i * 25 + 5)
+  .attr("y", (d, i) => svgHeight - yPercent(d) - 10)
   .attr("width", (d) => d)
   .attr("height", 25)
   .attr("font-size", "20px")
