00c035729126bd67646b248c683955cdcdee3a91
dasom222g <dasom228@gmail.com>
2025-04-14 11:47:31 +0900

    feat: bar차트 출력

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-const dataset = [10, 20, 30, 40, 50];
+const dataset = [100, 200, 300, 400, 500];
+
+const bgColors = ["red", "green", "blue", "pink", "aqua"];
+
+const fiveSvg = document.querySelector("svg.five");
+fiveSvg.style.width = `550px`;
+fiveSvg.style.height = `${30 * dataset.length}px`;
 
 d3.select("svg") // 그림을 그릴 svg캔버스 불러오기
   .selectAll("text") // 앞으로 생성할 여러가지 text요소 준비
@@ -24,6 +30,65 @@ d3.select("svg.second") // 그림을 그릴 svg캔버스 불러오기
   .append("text") // 준비된 요소를 text형태로 svg안에 추가
   .text((d) => d) // 기존 배열 데이터를 기준으로 값을 조작
   .attr("x", (d, i) => i * 40 + 10) // 가로축(x축) 위치값 설정
-  .attr("y", 10) // 세로축(y축) 위치값 설정
+  .attr("y", 20) // 세로축(y축) 위치값 설정
   .attr("font-size", "15px")
   .attr("fill", "green");
+
+/**
+ * 데이터 입력시 text요소는 y축을 기준으로 위쪽에 글이 배치됨
+ * 반면 rect와 같은 box요소는 y축을 기준으로 아래쪽에 배치됨
+ */
+
+// 가로 bar형태의 차트
+d3.select("svg.third")
+  .selectAll("rect")
+  .data(dataset)
+  .enter()
+  .append("rect")
+  .text((d) => d)
+  .attr("x", 0)
+  .attr("y", (d, i) => i * 30 + 0)
+  .attr("width", (d) => d)
+  .attr("height", 25)
+  .attr("fill", "green");
+
+// 세로bar
+const chartH = 600;
+d3.select("svg.fourth")
+  .selectAll("rect")
+  .data(dataset)
+  .enter()
+  .append("rect")
+  .text((d) => d)
+  .attr("x", (d, i) => i * 30 + 0)
+  .attr("y", (d, i) => chartH - d - 10) // bar를 하단에 배치, 10만큼의 간격
+  .attr("width", 25)
+  .attr("height", (d) => d)
+  .attr("fill", "orange");
+
+// 가로 bar형태의 차트 옆에 값 출력
+d3.select("svg.five")
+  .selectAll("rect")
+  .data(dataset)
+  .enter()
+  .append("rect")
+  .text((d) => d)
+  .attr("x", 0)
+  .attr("y", (d, i) => i * 30 + 0)
+  .attr("width", (d) => d)
+  .attr("height", 25)
+  .attr("fill", (_, i) => bgColors[i]); // 컬러 동적으로 변경
+
+// svg안쪽에 text요소 추가 출력
+d3.select("svg.five")
+  .selectAll("text")
+  .data(dataset)
+  .enter()
+  .append("text")
+  .text((d) => d)
+  .attr("x", (d) => d + 10)
+  .attr("y", (d, i) => i * 30 + 10 + 10)
+  .attr("width", (d) => d)
+  .attr("height", 25)
+  .attr("font-size", "20px")
+  .attr("fill", "black");
